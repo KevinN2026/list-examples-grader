aed8c65ac26fb5717004f8c9a2e6785bf876c2ae
KevinN2026 <130256471+KevinN2026@users.noreply.github.com>
2023-05-25 11:09:23 -0700

    Update

--- grade.sh
+++ grade.sh
@@ -8,18 +8,23 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if [[-e ./list-examples-grader/student-submission ]]
+if [[ -f student-submission/ListExamples.java ]]
     then
         echo "File exists!"
-        exit 1
     else 
         echo "File doesn't exist!"
+        exit 1
 fi
 
-cp ListExamples.java ListTests.java ~/OneDrive/Documents/GitHub/list-examples-grader/grading-area
+# cp ListExamples.java ListTests.java ~/OneDrive/Documents/GitHub/list-examples-grader/grading-area
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
 
-javac ListExamples.java ListTests.java
-java ListTests
+cd grading-area
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > TestResults.txt
+
 
 
 
